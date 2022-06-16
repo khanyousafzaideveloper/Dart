b6dd73be4910da9a617b3d15a8dff264f56e9a29
khanyousafzaideveloper <khanyousafzaideveloper@gmail.com>
2022-06-16 12:03:16 +0500

    .

---
+++ dynamic.dart
@@ -0,0 +1,5 @@
+void main() {
+  // dynamic is used to declare variable whose type is dynamic
+  dynamic variable = 'hello world!';
+  print(variable);
+}
---
+++ for_loop.dart
@@ -0,0 +1,13 @@
+void main() {
+  // numbers from 0 t0 10;
+  for (var i = 0; i < 10; i++) {
+    print(i);
+  }
+  // addition of numbers from 0 to 10;
+  var sum = 0;
+  for (var x = 0; x < 10; x++) {
+    sum = x + sum;
+  }
+  print("the sum of numbers is: ");
+  print(sum);
+}
